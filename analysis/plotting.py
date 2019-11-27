7d2c31a09130dbd237a089f7199c2b957e0c4c38
ahmedqamesh <ahmed.qamesh@yahoo.com>
2019-11-27 13:31:08 +0100

    rt commit

--- analysis/plotting.py
+++ analysis/plotting.py
@@ -37,11 +37,6 @@ from matplotlib.collections import PatchCollection
 from analysis import analysis
 from analysis import logger
 colors = ['black', 'red', '#006381', "blue", '#33D1FF', '#F5A9BC', 'grey', '#7e0044', 'orange', "maroon", 'green', "magenta", '#33D1FF', '#7e0044', "yellow"]
-
-# matplotlib.rc('text', usetex=True)
-# params = {'text.latex.preamble': [r'\usepackage{siunitx}']}
-# plt.rcParams.update(params)
-
 an =analysis.Analysis()
 
 class Plotting(object):     
@@ -49,56 +44,11 @@ class Plotting(object):
     def __init__(self):
         self.log = logger.setup_derived_logger('Plotting')
         self.log.info('Plotting initialized')
-        
-    def plot_linear(self, Directory=False, colors=colors, PdfPages=False, text=False, txt="Text",
-                     x=np.arange(1, 10), x_label="Supply current I_s [A]", y=np.arange(1, 10), y_label="Needed Voltage U_S [V]",
-                     map=False, z=np.arange(1, 10), z_label="Transferred Efficiency", test="DCConverter", title="powerSupply_Voltage", p=[1, 2, 3],
-                     line=None, data_line=[0], data_label='Power loss in the cable $P_c$ [W]'):
-        '''
-        PLot a relation between two variables 
-        '''
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        if map:
-            cmap = plt.cm.get_cmap('viridis', 15)
-            sc = ax.scatter(x, y, c=z, cmap=cmap, s=8)
-            cbar = fig.colorbar(sc, ax=ax, orientation='horizontal')
-            cbar.ax.invert_xaxis()
-            cbar.set_label(z_label, labelpad=1, fontsize=10)
-            plt.axvline(x=4, linewidth=0.8, color=colors[1], linestyle='dashed')
-            plt.axhline(y=22.5, linewidth=0.8, color=colors[1], linestyle='dashed')
-        else:
-            sc = ax.errorbar(x, y, xerr=0.0, yerr=0.0, fmt='o', color=colors[1], markersize=3, ecolor='black')
-            ax.plot(x, y, linestyle="-", color=colors[0], label="Fit", markersize=1)       
-            # ax.legend(loc="upper right")
-            # ax.set_ylim(ymin=min(y)-10)
-        ax.set_xlabel(x_label, fontsize=10)
-        ax.set_title(title, fontsize=8)
-        ax.set_ylabel(y_label, fontsize=10)
-        ax.ticklabel_format(useOffset=False)
-        ax.grid(True)
-        if text:
-            ax.text(0.95, 0.45, txt, fontsize=8,
-                    horizontalalignment='right', verticalalignment='top', transform=ax.transAxes,
-                    bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.2))
-                            
-        if line:
-            # Create axes for loss and voltage 
-            ax2 = ax.twinx()
-            line = ax2.errorbar(x, data_line, xerr=0.0, yerr=0.0, fmt='o', color=colors[3], markersize=1, ecolor='black')  # plot power loss
-            ax2.yaxis.label.set_color(colors[3])
-            ax2.tick_params(axis='y', colors=colors[3])
-            ax2.spines['right'].set_position(('outward', 3))  # adjust the position of the second axis 
-            ax2.set_ylabel(data_label, rotation=90, fontsize=10)
-            
-        fig.savefig(Directory + "/output/" + test + ".png", bbox_inches='tight')
-        plt.tight_layout()
-        PdfPages.savefig()
 
     def diode_calibration(self, diodes=["A"], Directory=False, PdfPages=False):
         '''
         The function plots the calibration results for the PN diodes
-        Input Directory = Dirctory + diode_calibration/cern_calibration/
+        Input Directory = Directory + diode_calibration/cern_calibration/
         diodes is an array of strings represents the diodes name A, B and C.
         '''
         self.log.info('Calibration results for the diodes')
@@ -387,7 +337,41 @@ class Plotting(object):
             plt.tight_layout()
             plt.savefig(Directory + "dose_current/" + depth[i] + '/dose_current_' + depth[i] + ".png", bbox_inches='tight')
             PdfPages.savefig()
+    def power_2d(self, Directory=False, PdfPages=False, size_I=50, size_V=60, V_limit=50, I_limit=50):
+        '''
+        Calculate the power in each point of I and V
+        '''
 
+        Power = np.zeros(shape=(size_I, size_V), dtype=float)
+        power_max = np.zeros(shape=(size_I, size_V), dtype=float)
+        p_max = V_limit * I_limit
+        V = np.arange(0, size_V, 1)
+        for i in range(size_I):
+            for v in range(len(V)):
+                Power[i, v] = i * v
+                if (i * v == p_max):
+                    power_max[i, v] = i * v
+        fig, ax = plt.subplots()
+        im = ax.imshow(Power, aspect='auto', origin='lower', interpolation='gaussian', cmap=plt.get_cmap('tab20c'))
+        cb = fig.colorbar(im, ax=ax, fraction=0.0594)
+        cb.set_label("Power [W]")
+        ax.set_xlabel('Voltage [kV]')
+        ax.set_ylabel('Current [mA]')
+        ax.set_xlim([0, len(V)])
+        ax.set_ylim([0, size_I])
+        ax.set_title('Power of x-ray tube ', fontsize=12)
+        ax.grid()
+        ax2 = ax.twinx()
+        x, y = np.where(power_max)
+        ax2.axis('off')
+        ax2.set_ylim([0, size_I])
+        plt.axhline(y=I_limit, linewidth=2, color='#d62728', linestyle='solid')
+        plt.axvline(x=V_limit, linewidth=2, color='#d62728', linestyle='solid')
+        plt.tight_layout()
+        plt.savefig(Directory + 'Power.png')
+        PdfPages.savefig()
+        
+        
     def dose_drop(self, Directory=False, PdfPages=False, stdev=0.06, depth=[0]):
         '''
         Dose drop after Al filter at
@@ -507,7 +491,253 @@ class Plotting(object):
             kafe_plot.plot_all(show_data_for='all', show_band_for=0)
             kafe_plot.save(Directory + test + "/dose_voltage/" + Depth + "/dose_voltage_" + Depth + "_kafe_Fit.png")
             PdfPages.savefig()
-            
+
+    def Plot_Beam_profile_2d(self, Directory=False, PdfPages=False, depth=None):
+        '''
+        Make a 2d scan at specific depth
+        '''
+        Factor = 9.847  # diode A
+        # Factor = 9.817  # diode B
+        Background = 5.7 * 10**(-9)  # nA
+        binwidth = 1
+        for d in range(len(depth)):
+            with tb.open_file(Directory + "without_Al_Filter/beamspot/" + depth[d] + "/beamspot_" + depth[d] + ".h5", 'r') as in_file:
+                beamspot = in_file.root.beamspot[:]
+                beamspot = (beamspot - Background) * 1000000 * Factor
+                mid_z, mid_x = np.int(beamspot.shape[0] / 2), np.int(beamspot.shape[1] / 2)
+            if (depth[d] == "3cm") or (depth[d] == "3cm_Vfilter") or (depth[d] == "3cm_Zrfilter"):
+                radius, r = r'$r=6.5 \pm 0.5$ mm', 1 * 6.5  # 1 mm step * radius
+                # l,w= 13, 6.5 # in mm
+            if depth[d] == "3cm_collimator":
+                radius, r = '$r=3.75 \pm 0.05$ mm\n$r_{collimator}$ =6 mm', 1 * 3.75
+            if depth[d] == "8cm":
+                radius, r = r'$r=10 \pm 0.5$ mm', 1 * 10.5  # 1 mm step * radius
+                l, w = 20, 11.5  # in mm
+            if depth[d] == "51cm":
+                radius, r = r'$r=40 \pm 0.5$ mm', 5 * 40  # 5 mm step * radius
+            if depth[d] == "60cm":
+                radius, r = r'$r=48 \pm 4$ mm', 48.4 / 5  # 5 mm step * radius
+            # put the mean value for each quad
+#             mean1 = np.round(np.mean(beamspot[np.int(beamspot.shape[0]/2-r):np.int(beamspot.shape[0]/2),np.int(beamspot.shape[1]/2):np.int(beamspot.shape[1]/2)+r]),3)
+#             mean2 = np.round(np.mean(beamspot[np.int(beamspot.shape[0]/2-r):np.int(beamspot.shape[0]/2),np.int(beamspot.shape[1]/2-r):np.int(beamspot.shape[1]/2)]),3)
+#             mean3 = np.round(np.mean(beamspot[np.int(beamspot.shape[0]/2):np.int(beamspot.shape[0]/2+r),np.int(beamspot.shape[1]/2-r):np.int(beamspot.shape[1]/2)]),3)
+#             mean4 = np.round(np.mean(beamspot[np.int(beamspot.shape[0]/2):np.int(beamspot.shape[0]/2+r),np.int(beamspot.shape[1]/2):np.int(beamspot.shape[1]/2+r)]),3)
+#             mean=[mean2,mean3,mean1,mean4]
+#
+#             y_offset = [np.int(beamspot.shape[0]/2-r),np.int(beamspot.shape[0]/2+r),np.int(beamspot.shape[0]/2-r),np.int(beamspot.shape[0]/2+r)]
+#             x_offset = [np.int(beamspot.shape[1]/2-r),np.int(beamspot.shape[1]/2-r),np.int(beamspot.shape[1]/2+r),np.int(beamspot.shape[1]/2+r)]
+#
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            cmap = plt.cm.get_cmap('viridis', 30)
+            im = ax.imshow(beamspot, aspect='auto', interpolation='gaussian', cmap=cmap)  # ,extent=extent)
+            cb = fig.colorbar(im, ax=ax, fraction=0.0594)
+            # create new axes on the right and on the top of the current axes
+            divider = make_axes_locatable(ax)
+            axHistx = divider.append_axes("top", 1.2, pad=0.2, sharex=ax)
+            axHisty = divider.append_axes("right", 1.2, pad=0.2, sharey=ax)
+            axHistx.bar(x=range(beamspot.shape[0]), height=np.ma.sum(beamspot, axis=0), align='center',
+                        linewidth=1, color=(0.2, 0.4, 0.6, 0.6), edgecolor='black')
+            axHistx.plot(range(beamspot.shape[0]), np.ma.sum(beamspot, axis=0), "black")
+            # make some labels invisible
+            plt.setp(axHistx.get_xticklabels() + axHisty.get_yticklabels(), visible=False)
+            axHistx.set_ylabel('Dose [$Mrad/hr$]', fontsize=10)
+            axHisty.set_xlabel('Dose [$Mrad/hr$]', fontsize=10)
+            axHisty.barh(y=range(beamspot.shape[1]), width=np.ma.sum(beamspot, axis=1), align='center',
+                         linewidth=1, color=(0.2, 0.4, 0.6, 0.6), edgecolor='black')
+            axHisty.plot(np.ma.sum(beamspot, axis=1), range(beamspot.shape[1]), "black")
+            cb.set_label("Dose rate [$Mrad/hr$]")
+            ax.text(1.5, 1.3, radius,
+                    horizontalalignment='right', verticalalignment='top', transform=ax.transAxes,
+                    bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8), fontsize=10)
+            plt.title("Beam profile at " + depth[d] + " from the collimator holder (%s and %s)" % ("40 kV", "50mA"), fontsize=12, y=1.7, x=-0.6)
+            ax.set_xlabel('x [mm]')
+            ax.set_ylabel('y[mm]')
+            plt.savefig(Directory + "without_Al_Filter/beamspot/" + depth[d] + "/beamspot_" + depth[d] + "_2d.png")
+            PdfPages.savefig()
+
+            fig, ax2 = plt.subplots()
+            central_value = beamspot[mid_z, mid_x]
+            for z in np.arange(beamspot.shape[0]):
+                for x in np.arange(beamspot.shape[1]):
+                    beamspot[z, x] = beamspot[z, x] / np.float(central_value) * 100
+            cmap2 = plt.cm.get_cmap('viridis', 5)
+            im2 = ax2.imshow(beamspot, aspect='auto', interpolation='gaussian', cmap=cmap2)
+            cb2 = fig.colorbar(im2, ax=ax2, fraction=0.0594)
+
+          #  for j, txt in enumerate(mean):
+          #      ax2.annotate(txt,xy=(x_offset[j],y_offset[j]),color='#d62728', size=8)
+            plt.axhline(y=mid_z, linewidth=0.5, color='#d62728', linestyle='dashed')
+            plt.axvline(x=mid_x, linewidth=0.5, color='#d62728', linestyle='dashed')
+
+            if (depth[d] == "8cm"):
+                # Draw module details
+                rec_position_x, rec_position_y = mid_z - w / 2, mid_x - l / 2
+                rect2 = patches.Rectangle((rec_position_x, rec_position_y), w, l, linewidth=2, edgecolor='black', facecolor='none')
+                ax2.add_patch(rect2)
+                plt.annotate(s='', xy=(rec_position_x - 0.5, mid_x + l / 2), xytext=(rec_position_x - 0.5, rec_position_y), arrowprops=dict(arrowstyle='<->'))
+                ax2.annotate(np.str(l) + "mm", xy=(rec_position_x - 5, rec_position_y + l / 2), color='white', size=10)
+
+                plt.annotate(s='', xy=(rec_position_x, rec_position_y - 0.5), xytext=(rec_position_x + w, rec_position_y - 0.5), arrowprops=dict(arrowstyle='<->'))
+                ax2.annotate(np.str(w) + "mm", xy=(rec_position_x + w / 2 - 1.5, rec_position_y - 1.0), color='white', size=10)
+
+            # draw a circle represents the central position
+            if depth[d] == "3cm_collimator":
+                circle2 = plt.Circle((mid_z, mid_x), r, color='red', fill=False)
+            else:
+                circle2 = plt.Circle((mid_z, mid_x - 1), r, color='red', fill=False)
+            ax2.add_artist(circle2)
+            ax2.add_artist(plt.Circle((mid_z, 2 * mid_x), 1., color='red'))
+            ax2.set_title("Beam profile at " + depth[d] + " from the collimator holder (%s and %s)" % ("40 kV", "50mA"),
+                          fontsize=11)
+            ax2.set_xlabel('x [mm]')
+            ax2.set_ylabel('y[mm]')
+            cb2.set_label("Relative intensity to central position [$\%$]")
+            plt.savefig(Directory + "without_Al_Filter/beamspot/" + depth[d] + "/beamspot_percentile" + depth[d] + "_2d.png")
+
+            PdfPages.savefig()
+
+    def Plot_Beam_profile_3d(self, Directory=False, PdfPages=False, depth=[0]):
+        '''
+        Make a 3d scan at specific depth (The function is under updates)
+        '''
+        def f(x, y, Factor=9.62, Background=0.012, beamspot=None):
+            return (beamspot[y, x] * 1000000 - Background) * Factor
+        for d in range(len(depth)):
+            with tb.open_file(Directory + "without_Al_Filter/beamspot/" + depth[d] + "/beamspot_" + depth[d] + ".h5", 'r') as in_file:
+                beamspot = in_file.root.beamspot[:]
+            y = np.linspace(0, beamspot.shape[0] - 1, 100, dtype=int)
+            x = np.linspace(0, beamspot.shape[1] - 1, 100, dtype=int)
+            X, Y = np.meshgrid(x, y)
+            Z = f(X, Y, beamspot=beamspot)
+
+            fig = plt.figure()
+            ax = fig.gca(projection='3d')
+            plot = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+            plt.axhline(y=25, linewidth=2, color='#d62728', linestyle='dashed')
+            plt.axvline(x=16, linewidth=2, color='#d62728', linestyle='dashed')
+            cb = fig.colorbar(plot, ax=ax, fraction=0.046)
+            cb.set_label("Dose rate [$Mrad/hr$]")
+            ax.set_xlabel('x [mm]')
+            ax.set_ylabel('y[mm]')
+            plt.axis('off')
+            ax.set_title("Beam profile at " + depth[d] + "without collimator support", fontsize=12)
+            plt.savefig(Directory + "without_Al_Filter/beamspot/" + depth[d] + "/beamspot_" + depth[d] + "_3d.png")
+            PdfPages.savefig()
+
+    def plot_beamspot(self, filename =False,PdfPages=False):
+        self.log.info('plot the beamspot')
+        with tb.open_file(filename, 'r') as in_file:
+            data = in_file.root.beamspot[:]
+                        
+        # Calibration Factor
+        Factor = 9.81  # diode B
+        #  Factor = 9.76 # diode A
+        Background = 5.7*10**(-9)  # 5 nA
+        data = (data-Background)*Factor*10**9
+        
+        # OF module
+        mod_length = 84.975/10.0
+        mod_width = 15.4/10.0
+        
+        # DHP
+        dhp_length = 4.2/10.0
+        dhp_width = 3.28/10.0
+        
+        # DCD
+        dcd_length = 5.13/10.0
+        dcd_width = 3.41/10.0
+        
+        # Switcher
+        sw_length = 3.6/10.0
+        sw_width = 1.89/10.0
+        sw_locs = [8.96, 9.92, 10.88, 10.88, 10.88, 9.235, 19.338]
+        
+        # module collection
+        mod_color = "black"
+        # module outline
+        mod_patches = [Rectangle((0, 0), mod_length, mod_width, fill=False, color=mod_color, linewidth=2)]
+        # dhps
+        for i in range(4):
+            mod_patches.append(Rectangle((mod_length-1.3494-dhp_length/2, mod_width-0.2759-dhp_width/2-0.35*i), dhp_length, dhp_width, fill=False, color=mod_color, linewidth=1.5))
+        # dcds
+        for i in range(4):
+            mod_patches.append(
+                Rectangle((mod_length - 1.9338 - sw_length / 2, mod_width - 0.2759 - dcd_width / 2 - 0.35 * i), dcd_length,
+                          dcd_width, fill=False, color=mod_color, linewidth=1.5))
+        # switchers
+        for i in range(6):
+            mod_patches.append(
+                Rectangle((mod_length - sum(sw_locs[:-i-3:-1])/10.0 - sw_length / 2, mod_width - 0.1278 - sw_width / 2), sw_length,
+                          sw_width, fill=False, color=mod_color, linewidth=1.5))
+        a_x1 = -mod_length/2 - 1.0 +1.3494-dhp_length/2
+        a_x2 = +mod_length/2 - 1.0
+        a_y1 = -mod_width/2 # - 1.0
+        a_y2 = +mod_width/2 # - 1.0
+        mod_trans_a = mpl.transforms.Affine2D().translate(-mod_length/2, -mod_width/2) + mpl.transforms.Affine2D().rotate_deg(180) + mpl.transforms.Affine2D().translate(-1.0,0)
+        p_mod = PatchCollection(mod_patches, match_original=True)
+        
+        nullfmt = NullFormatter()         # no labels
+        
+        # definitions for the axes
+        left, width = 0.1, 0.60
+        bottom, height = 0.1, 0.60
+        bottom_p = bottom + height + 0.02
+        left_p = left + width + 0.02
+        
+        rect_spot = [left, bottom, width, height]
+        rect_projx = [left, bottom_p, width, 0.2]
+        rect_projy = [left_p, bottom, 0.2, height]
+        
+        # start with a rectangular Figure
+        plt.figure() # 1, figsize=(8, 8))
+        
+        axSpot = plt.axes(rect_spot)
+        axProjx = plt.axes(rect_projx)
+        axProjy = plt.axes(rect_projy)
+        
+        # no labels
+        axProjx.xaxis.set_major_formatter(nullfmt)
+        axProjy.yaxis.set_major_formatter(nullfmt)
+        
+        # the scatter plot:
+        im_spot = axSpot.imshow(data, origin="upper", interpolation="None", aspect="auto", extent=(-6.5, +6.5, +6.5, -6.5))
+        
+        axSpot.plot((-6.5, +6.5), (0, 0), color="white", lw=1)
+        axSpot.plot((0, 0), (-6.5, +6.5), color="white", lw=1)
+        p_mod.set_transform(mod_trans_a+axSpot.transData)
+        axSpot.add_collection(p_mod)
+        axSpot.set_xlabel("x [cm]")
+        axSpot.set_ylabel("z [cm]")
+        axrange=np.arange(-6, +6.5, 0.5)
+        
+        axProjx.bar(x=axrange, height=data[12, :].astype(np.float), width=0.3)
+        axProjx.set_ylabel("dose rate [krad/h]")
+        axProjx.axvline(a_x1, color=mod_color)
+        axProjx.axvline(a_x2, color=mod_color)
+        
+        axProjy.barh(y=axrange, width=data[:, 12].astype(np.float), height=0.3)
+        axProjy.axhline(a_y1, color=mod_color)
+        axProjy.axhline(a_y2, color=mod_color)
+        axProjy.set_xlabel("dose rate [krad/h]")
+        
+        axProjx.set_xlim(axSpot.get_xlim())
+        axProjy.set_ylim(axSpot.get_ylim())
+        
+        axProjy.set_xlim(axProjx.get_ylim())
+        
+        textleft = 'PXD outer forward module'
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        axSpot.text(0.05, 0.95, textleft, transform=axSpot.transAxes, fontsize=8, verticalalignment='top', bbox=props)
+        
+        cbar = plt.colorbar(im_spot)
+        cbar.set_label("dose rate [krad/h]")
+        
+        plt.suptitle("beamspot at distance 60 cm, tube parameter: 40 kV, 50 mA, no filter")
+        
+        plt.savefig(filename[:-3]+".png")
+        plt.savefig(filename[:-3]+".pdf")                       
+        PdfPages.savefig()
        
     def close(self, PdfPages=False):
             PdfPages.close()
@@ -523,8 +753,9 @@ class Attenuation(object):
         for target in targets:
             fig = plt.figure()
             ax = fig.add_subplot(111)
-            data = loadtxt(Directory + target + 
-                           "/mass_attenuation_coeff_in_detail_" + target + ".dat")
+            print(Directory + target + "/mass_attenuation_coeff_in_detail_" + target + ".dat")
+            data = loadtxt(Directory + target + "/mass_attenuation_coeff_in_detail_" + target + ".dat")
+            
             x = data[:, 0]  # Energy in Kev
             # total mass attenuation coeff with coherent scattering
             y = data[:, 6]
