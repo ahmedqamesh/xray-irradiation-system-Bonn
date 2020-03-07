5cf0ac0a3cf8784de7675b106229b2f9a4b38af0
ahmedqamesh <ahmedqamesh@github.com>
2020-03-07 19:38:51 +0100

    Fit functions using yaml file

--- analysis/fitEquations.py
+++ analysis/fitEquations.py
@@ -7,15 +7,6 @@ rootdir = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(rootdir[:-8], "Xray_irradiation_conf.yaml")
 with open(filename, 'r') as ymlfile:
     conf = yaml.load(ymlfile, Loader=yaml.FullLoader)
-dose_c = conf["FitFunctions"]["dose_current"]
-c_array =[dose_c[i] for i in ["with"] if i in dose_c]
-
-c = c_array[0]["3cm"]
-print(c)
-cv_array =[c[i] for i in ["30kV"] if i in c]
-print(cv_array[0])
-
-        
 
 def linear(x, m, c):
     return m * x + c
@@ -29,125 +20,47 @@ def Inverse_square(x, a, b, c):
 def quadratic(x, a, b, c):
     return a*x**2+b*x+c
 
-def opening_angle(h=None, r=None):
+def opening_angle(h=None, r=None, conf = conf):
     #The function will return  radius if h is given 
     #The function will return  height if r is given
-    #r = hx+c
-    #m=0.072+/-0.002
-    #c=0.0451+/-0.030    
+    opening_angle = conf["FitFunctions"]["opening_angle"] 
     if h is not None:
-        result = linear(h,0.072,0.451)
+        result = linear(h,opening_angle["a"],opening_angle["b"])
     if r is not None:
-        result = inv_linear(r,0.072,0.451)
+        result = inv_linear(r,opening_angle["a"],opening_angle["b"])
     return result
 
-def dose_current(I=None, d= None, depth ="3cm", filter= "without", voltage = "40kV"):
+def dose_current(I=None, d= None, depth ="3cm", filter= "without", voltage = "40kV", conf = conf):
     #The function will return  dose if I is given 
     #The function will return  current if d is given
+    dose_current = conf["FitFunctions"]["dose_current"][filter][depth][voltage]
     if I is not None:
-        if filter == "without":
-            if depth == "3cm":
-                if voltage == "40kV":
-                    result=linear(I,0.29,-0.14)
-                if voltage == "30kV":
-                    result=linear(I,0.24,-0.12)
-                             
-            if depth == "5cm":
-                if voltage == "40kV":
-                   result=linear(I,0.22,-0.06)
-                if voltage == "30kV":
-                   result=linear(I,0.18,-0.09)
-                
-            if depth == "8cm":
-                if voltage == "40kV":
-                    result=linear(I,0.14,-0.05)
-                if voltage == "30kV":
-                    result=linear(I,0.11,-0.03)
-                                        
-        if filter == "Al":
-            if depth == "3cm":
-                if voltage == "40kV":
-                   result=linear(I,0.08,0)
-                if voltage == "30kV":
-                   result=linear(I,0.06,-0.01)
-                    
-            if depth == "5cm":
-                if voltage == "40kV":
-                   result=linear(I,0.06,-0.02)
-                if voltage == "30kV":
-                   result=linear(I,0.05,0.0)
-            
-            if depth == "8cm":
-                if voltage == "40kV":
-                    result=linear(I,0.04,-0.01)
-                if voltage == "30kV":
-                    result=linear(I,0.03,-0.01)
-                
+        result=linear(I,dose_current["a"],dose_current["b"])
     if d is not None:
-        if filter == "without":
-            if depth == "3cm":
-                result=inv_linear(d,0.29,-0.14)
-            
-            if depth == "5cm":
-                print("Not yet")
-
-            if depth == "8cm":
-                result=inv_linear(d,0.15,-0.01)
-                    
-        if filter == "Al":
-            if depth == "3cm":
-                result=inv_linear(d,0.08,0)
-    
-            if depth == "5cm":
-                print("Not yet")
-
-            if depth == "8cm":
-                result=inv_linear(d,0.04,-0.05)
+        result=inv_linear(d,dose_current["a"],dose_current["b"])
     return result            
 
-
-def dose_voltage(V=None, d= None, current ="10A", filter= "without",  depth ="8cm"):
+def dose_voltage(V=None, d= None, current ="10mA", filter= "without",  depth ="8cm", conf = conf):
+    dose_voltage =conf["FitFunctions"]["dose_voltage"][filter][depth][current]
     #The function will return  dose if V is given 
     if filter == "without":
-        if current == "10A":
-            result=quadratic(V,-0.001,0.068,-0.404)
-
-        if current == "20A":
-            result=quadratic(V,-0.001,0.139,-0.919)
-        
-        if current == "30A":
-            result=quadratic(V,-0.002,0.208,-1.423)
-                            
-        if current == "40A":
-            result=quadratic(V,-0.002,0.279,-1.932)                            
-        else: print("Not yet")  
+        result=quadratic(V,dose_voltage["a"],dose_voltage["b"],dose_voltage["c"])
     else:   print("Not yet")
-        
     return result 
 
-def dose_depth(h = None, filter = "without"):
-    if h is not None:
-        if filter == "without":
-            result = Inverse_square(h,1391.77,6.20,0.13)
-        
-        if filter == "Al":
-            result = Inverse_square(h,431.19,6.75,0.03)
-    if h is None: 
-        if filter == "without":
-            R2 = 1 
-            h2 = 2
-            result = R2*((h2-6.75)/(h-6.75))**2
-        if filter == "Al":
-            R2 = 1 
-            h2 = 2
-            result = R2*((h-6.75)/(h2-6.75))**2
-                    
+def dose_depth(depth = None, dose = None, filter = "without", conf = conf, current ="50mA",voltage = "40kV" ):
+    dose_voltage =conf["FitFunctions"]["dose_depth"][filter][voltage][current]
+    if depth is not None:
+        result = Inverse_square(depth,dose_voltage["a"],dose_voltage["b"],dose_voltage["c"])
+    if depth is None: 
+        result = np.sqrt(dose_voltage["a"]*(dose+dose_voltage["c"])**(-1))-dose_voltage["b"]
     return result
 if __name__ == '__main__':
     r = opening_angle(h = 60)
     h = opening_angle(r = 5)
-    dC = dose_current(I = 40, filter = "Al", depth ="3cm")
-    dose = dose_current(d = 15, filter = "Al", depth ="3cm")
+    dC = dose_current(I = 40, filter = "without", depth ="3cm",voltage = "40kV")
+    dose = dose_current(d = 11.4, filter = "without", depth ="3cm",voltage = "40kV")
     dV = dose_voltage(V = 30, filter = "without", depth ="8cm")
-    dD = dose_depth(h = 30, filter = "without")
-    print (r, h,dC,dV, dD, dose)
+    dD = dose_depth(depth = 40, filter = "without")
+    dD2 = dose_depth(dose = 0.522053934521467, filter = "without",current ="50mA",voltage = "40kV")
+    print (r, h,dC,dV, dD, dose, dD2)
